c7d2c2bacb17844a382850fc5b638fd8b6bd3c7b
Njovane980 <vuyisekanjovane8@gmail.com>
2024-02-26 11:40:26 +0200

    FolderStructureUpdated

---
+++ frontend/src/router/index.js
@@ -0,0 +1,37 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: () => import('../views/AboutView.vue')
+  },
+  {
+    path: '/products',
+    name: 'Products',
+    component: () => import('../views/ProductsView.vue')
+  },
+  {
+    path: '/admin',
+    name: 'Admin',
+    component: () => import('../views/AdminView.vue')
+  },
+  {
+    path: '/contact',
+    name: 'Contact',
+    component: () => import('../views/ContactView.vue')
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <!-- <img alt="HomePage" src="https://i.postimg.cc/zGHjZL8y/Successful-Butchery-01-1.webp"> -->
+    
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'HomeView',
+  components: {
+    
+  }
+}
+</script>
