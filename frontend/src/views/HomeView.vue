b33166954d59d5df5cf0437f6037b415921d74e5
Njovane980 <vuyisekanjovane8@gmail.com>
2024-02-26 11:57:49 +0200

    Home page - Update

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,7 +1,9 @@
 <template>
-  <div class="home">
-    <!-- <img alt="HomePage" src="https://i.postimg.cc/zGHjZL8y/Successful-Butchery-01-1.webp"> -->
-    
+  <div class="container">
+    <img alt="HomePage" src="https://i.postimg.cc/zGHjZL8y/Successful-Butchery-01-1.webp">
+    <div class="row">
+      <h2 class="display-2">Home</h2>
+    </div>
   </div>
 </template>
 
