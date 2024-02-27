601c80033dd06263ea9d0e243b2ea548fb76c01f
Njovane980 <vuyisekanjovane8@gmail.com>
2024-02-27 11:22:13 +0200

    AboutPage

--- frontend/src/views/ProductsView.vue
+++ frontend/src/views/ProductsView.vue
@@ -1,15 +1,43 @@
 <template>
-    <div>
-<h2>this is a product page</h2>
+    <div class="about">
+      <img src="https://i.postimg.cc/fLB8wVXt/wepik-photo-mode-2022715-143357.webp" alt="Image2" class="img2">
     </div>
-</template>
-
-<script>
-    export default {
-        
-    }
-</script>
-
-<style scoped>
-
-</style>
+    <h2 class="display-3">Products</h2>
+    <div>
+  </div>
+  <div>
+    
+  </div>
+  </template>
+  
+  <style>
+    .about {
+    position: relative;
+  }
+  
+  .img2 {
+    width: 100%;
+    height: 200px;
+    display: block;
+    object-fit: cover;
+  }
+  
+  .display-3 {
+    position: absolute;
+    top: 100px; 
+    left: 90%;
+    transform: translateX(-50%);
+    color: white;
+    font-size: 5em;
+    background-color: rgba(0, 0, 0, 0.386);
+    z-index: 1;
+  }
+  
+  .para2 {
+    text-align: center;
+    margin-top: 70px; 
+    font-size: 1.5em;
+  
+  }
+  
+  </style>
